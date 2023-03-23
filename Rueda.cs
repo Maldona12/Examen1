93ffbb7cad19a834a67ca8c1e36c9e0ce2dab6eb
Uziel_M <uzielmalquint7@gmail.com>
2023-03-22 22:23:28 -0600

    Ajustes

--- Rueda.cs
+++ Rueda.cs
@@ -2,11 +2,12 @@ namespace Examen1
 {
     class Rueda {
         public Random random = new Random();
-        public int numero;
-        public string color;
-        public string paridad;
-        List<int> numerosRojo = new List<int> {2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35};
-        List<int> numerosNegro = new List<int> {1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36};
+        public int numero { get; }
+        public string color { get; }
+        public string paridad { get; }
+
+        private static readonly int[] numerosRojo = {2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35};
+        private static readonly int[] numerosNegro = {1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36};
 
         public Rueda() {
             this.numero = random.Next(0, 37); // Genera un número aleatorio entre 0 y 36
@@ -32,12 +33,8 @@ namespace Examen1
             return "Sin color asignado";
         }
 
-        public string determinarParidad() {
-            if (numero % 2 == 0) {
-                return "Par";
-            } else {
-                return "Impar";
-            }
+         public string determinarParidad() {
+            return numero % 2 == 0 ? "Par" : "Impar";
         }
     }
 }
